ab9412337510a150a2c3bfab1d09b30b2e4486a1
Mouhammad kharboutli <eng.mouhammad.kharboutli@gmail.com>
2026-01-06 11:35:51 +0100

    added gitignore files

--- 1.NumberAnalyzer/NumberAnalyzer/obj/Debug/net9.0/NumberAnalyzer.AssemblyInfo.cs
+++ 1.NumberAnalyzer/NumberAnalyzer/obj/Debug/net9.0/NumberAnalyzer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberAnalyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21ac238836f9f49b6ecfd9748a8c1108370f051")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberAnalyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberAnalyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
